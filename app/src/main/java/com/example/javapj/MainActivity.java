62786979525b2b453c907831683b853c72863451
Francesco Fico <fficusbenjamin@gmail.com>
2021-02-02 19:06:23 +0000

    switch implemented

--- app/src/main/java/com/example/javapj/MainActivity.java
+++ app/src/main/java/com/example/javapj/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.javapj;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import com.example.javapj.Tests.*;
@@ -22,18 +23,33 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void allTests(View view) throws IOException {
-        //Base64Test base64 = new Base64Test();
-        //MatMulTest.main();
-        BrainTest.main();
+        Switch base64Sw = (Switch) findViewById(R.id.base64Switch);
+        Switch brainfSw = (Switch) findViewById(R.id.brainSwitch);
+        Switch matSw = (Switch) findViewById(R.id.matmulSwitch);
 
-        System.out.println("CLICKED");
+        if (base64Sw.isChecked()){
+            System.out.println("BASE64");
+            Base64Test base64 = new Base64Test();
+            base64.main();
+        }
+
+        if(brainfSw.isChecked()){
+            System.out.println("BRAINTEST");
+            BrainTest.main();
+        }
+
+        if(matSw.isChecked()){
+            System.out.println("MATMUL");
+            MatMulTest.main();
+        }
+
         TextView info = (TextView) findViewById(R.id.showDeviceInfo);
-        //info.setText(DeviceInfo.getFullDeviceName());
+        info.setText(DeviceInfo.getFullDeviceName());
         //info.setText(MatMul.);
         //showTestResult.setText(Tests.overallTime);
         //overall.setText(results);
         //info.setText(Base64Java.results);
-        //base64.main();
+
 
 
     }
